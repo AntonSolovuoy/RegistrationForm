cad47335382c901345023f1282a67047a6d63331
Anton <anton_solovuoy@mail.ru>
2017-02-14 13:56:45 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 import SignIN from "./src/SignIN";
 import SignUP from "./src/SignUP";
 
 
 
 ReactDOM.render ((
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={SignIN} />
     <Route path="/registration" component={SignUP} />
   </Router>
